7c0cd08b7d47af16bf680bd7f6062eccfaf7166f
Devin <ospreywebservices@gmail.com>
2021-05-23 23:52:12 -0400

    added buttons to display navbar directory in interface

--- src/modules/UserInterface.js
+++ src/modules/UserInterface.js
@@ -1,12 +1,39 @@
+import { isToday } from 'date-fns';
 import { createTask } from './TaskFunctions.js';
 
 function initializeHomepage() {
-    
+
 }
+
+const navbarButtonController = (() => {
+    inbox.addEventListener('click', () => {
+        currentTitle.textContent = 'Inbox'
+    })
+    today.addEventListener('click', () => {
+        currentTitle.textContent = 'Today'
+    })
+    week.addEventListener('click', () => {
+        currentTitle.textContent = 'Weekly'
+    })
+    month.addEventListener('click', () => {
+        currentTitle.textContent = 'Monthly'
+    })
+    important.addEventListener('click', () => {
+        currentTitle.textContent = 'Important'
+    })
+
+    taskTitle.addEventListener('click', () => console.log('hi'))
+})();
+
+
+
+
+
+
+
 
 //testing task factory
 const taskbro = new createTask('Mow', '', '', '')
-
 console.log(taskbro)
 
 export {
